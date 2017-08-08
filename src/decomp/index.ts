a12967e0a0ee3b4ac9dd682d1092f633adfcc519
summivox <summivox@gmail.com>
2017-08-08 10:25:26 -0700

    wip decompTenpai, getting too complicated

--- src/decomp/index.ts
+++ src/decomp/index.ts
@@ -3,6 +3,8 @@ import { TenpaiType } from "./tenpai-type";
 import * as k7 from './k7';
 import * as lookup from "./lookup";
 const {complete: lookupComplete, waiting: lookupWaiting} = lookup;
+import * as packedMentsu from "../packed-mentsu";
+const {concatOffset: concat} = packedMentsu;
 
 export interface TenpaiDecomp {
     type: TenpaiType;
@@ -24,7 +26,7 @@ const kokushi13Decomps = yaochuu.map(tenpai => <TenpaiDecomp>{
     type: TenpaiType.kokushi13, tenpai, anchor: tenpai, menjan: 0
 });
 
-export function decompTenpai(suites: ArrayLike<number>) {
+export function decompTenpai(suites: ArrayLike<number>): TenpaiDecompSet {
     // kokushi before everything else
     const kokushi = k7.kokushi(suites);
     switch (kokushi) {
@@ -41,56 +43,109 @@ export function decompTenpai(suites: ArrayLike<number>) {
             };
     }
 
+    const tenpai = new Uint16Array(4);
+    const decomps: TenpaiDecomp[] = [];
+
     std: do {
         // tsuupai: handle specially because there can't be shuntsu
         let suiteZ = suites[3];
         let typeZ = TenpaiType.kokushi; // use obviously invalid for default
         let j1 = 0, j2 = 0;
-        let mentsuZ = 0;
+        let j1s, j1n, j2s, j2n;
+        let mentsuZ = 0, shiftZ = 1;
         for (let i = 30; suiteZ !== 0; ++i, suiteZ >>= 3) {
             switch (suiteZ & 0o7) {
                 case 0: break;
                 case 1:
                     if (j1 === 0) {
-                        j1 = i;
+                        j1 = i; j1s = 3; j1n = i - 30;
                         typeZ = TenpaiType.tanki;
                     } else break std;
                     break;
                 case 2:
                     if (j1 === 0) {
-                        j1 = i;
+                        j1 = i; j1s = 3; j1n = i - 30;
                         typeZ = TenpaiType.shanpon;
                     } else if (j2 === 0) {
                         if (typeZ !== TenpaiType.shanpon) break std;
-                        j2 = i;
+                        j2 = i; j2s = 3; j2n = i - 30;
                     } else break std;
                     break;
                 case 3:
-                    // INLINE: packedMentsu.push(mentsuZ, i, 0)
-                    mentsuZ = (mentsuZ << 6) | (i << 1);
+                    // populate in inverse order for direct concat with MPS later
+                    mentsuZ |= i << shiftZ;
+                    shiftZ += 6;
                     break;
                 case 4: break std;
             }
         }
-        const csM = lookupComplete.get(suites[0]);
-        const csP = lookupComplete.get(suites[1]);
-        const csS = lookupComplete.get(suites[2]);
+        const menjansM = lookupComplete.get(suites[0]);
+        const menjansP = lookupComplete.get(suites[1]);
+        const menjansS = lookupComplete.get(suites[2]);
         if (typeZ !== TenpaiType.kokushi) {
             // must be tanki Z, shanpon MPS-Z, or shanpon Z-Z
             // MPS must be complete
-            if (csM == null || csP == null || csS == null) break std;
+            if (menjansM == null || menjansP == null || menjansS == null) break std;
             // MPS must contain at most 1 pair
-            const jM = csM[0] & 0xf;
-            const jP = csP[0] & 0xf;
-            const jS = csS[0] & 0xf;
-            const jMPS = +(jM !== 0) + +(jP !== 0) + +(jS !== 0);
+            const jM = menjansM[0] & 0xf, jMExist = jM !== 0;
+            const jP = menjansP[0] & 0xf, jPExist = jP !== 0;
+            const jS = menjansS[0] & 0xf, jSExist = jS !== 0;
+            const jMPS = +jMExist + +jPExist + +jSExist;
             if (jMPS > 1) break std;
-            // MPS contain exactly 1 pair => must be shanpon MPS-Z
             if (jMPS === 1) {
+                // MPS contain exactly 1 pair => must be shanpon MPS-Z
                 if (j2 === 0) {
                     if (typeZ !== TenpaiType.shanpon) break std;
-                    j2 = jM + jP + jS;
+
+                    //////////////////////////////////////////////////////////////////////////////
+                    // FIXME: this is wrong --- jM/P/S can change as we loop over menjanM/P/S
+                    //////////////////////////////////////////////////////////////////////////////
+                    if (jMExist) { j2 = jM; j2s = 0; j2n = jM - 1; }
+                    else if (jPExist) { j2 = jP + 10; j2s = 1; j2n = jM - 1; }
+                    else if (jSExist) { j2 = jS + 20; j2s = 2; j2n = jM - 1; }
+
                 } else break std;
+            }
+            // loop over complete suites
+            for (let iS = 0, ieS = menjansM.length; iS < ieS; ++iS) {
+                const menjanS = menjansS[iS];
+                const mentsuSZ = concat(mentsuZ, menjanS >> 4, 40);
+                for (let iP = 0, ieP = menjansM.length; iP < ieP; ++iP) {
+                    const menjanP = menjansP[iP];
+                    const mentsuPSZ = concat(mentsuSZ, menjanP >> 4, 20);
+                    for (let iM = 0, ieM = menjansM.length; iM < ieM; ++iM) {
+                        const menjanM = menjansM[iM];
+                        const mentsuMPSZ = concat(mentsuSZ, menjanM >> 4, 0);
+                        switch (typeZ) {
+                            case TenpaiType.tanki:
+                                decomps.push({
+                                    type: TenpaiType.tanki,
+                                    tenpai: j1,
+                                    anchor: j1,
+                                    menjan: (mentsuMPSZ << 4),
+                                });
+                                tenpai[j1s] |= 1 << j1n;
+                                break;
+                            case TenpaiType.shanpon:
+                                decomps.push({
+                                    type: TenpaiType.shanpon,
+                                    tenpai: j1,
+                                    anchor: j1,
+                                    menjan: (mentsuMPSZ << 4) | j2,
+                                }, {
+                                    type: TenpaiType.shanpon,
+                                    tenpai: j2,
+                                    anchor: j2,
+                                    menjan: (mentsuMPSZ << 4) | j1,
+                                });
+                                tenpai[j1s] |= 1 << j1n;
+                                tenpai[j2s] |= 1 << j2n;
+                                break;
+                            default:
+                                throw Error("WTF");
+                        }
+                    }
+                }
             }
         } else {
 
